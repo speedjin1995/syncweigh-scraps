7aea53da3d24e4baab7677ffa4a788f64c0d7697
Ng Jih Bin <sky.ng@dcap.my>
2026-07-02 12:55:44 +0800

    change host to localhost instead

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
-$db = mysqli_connect("srv597.hstgr.io", "u664110560_treer", "@Sync5500", "u664110560_treer");
+$db = mysqli_connect("localhost", "u664110560_treer", "@Sync5500", "u664110560_treer");
 
 if(mysqli_connect_errno()){
     echo 'Database connection failed with following errors: ' . mysqli_connect_error();
